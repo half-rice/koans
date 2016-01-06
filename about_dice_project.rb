9a47426c83a039768024a1b1f6fae86ae38ea487
surrus <xsurrus@gmail.com>
2016-01-06 08:33:31 -0700

    completed

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -17,29 +17,29 @@ class DiceSet
 end
 
 class AboutDiceProject < Neo::Koan
-  # def test_can_create_a_dice_set
-  #   dice = DiceSet.new
-  #   assert_not_nil dice
-  # end
+  def test_can_create_a_dice_set
+    dice = DiceSet.new
+    assert_not_nil dice
+  end
 
-  # def test_rolling_the_dice_returns_a_set_of_integers_between_1_and_6
-  #   dice = DiceSet.new
+  def test_rolling_the_dice_returns_a_set_of_integers_between_1_and_6
+    dice = DiceSet.new
 
-  #   dice.roll(5)
-  #   assert dice.values.is_a?(Array), "should be an array"
-  #   assert_equal 5, dice.values.size
-  #   dice.values.each do |value|
-  #     assert value >= 1 && value <= 6, "value #{value} must be between 1 and 6"
-  #   end
-  # end
+    dice.roll(5)
+    assert dice.values.is_a?(Array), "should be an array"
+    assert_equal 5, dice.values.size
+    dice.values.each do |value|
+      assert value >= 1 && value <= 6, "value #{value} must be between 1 and 6"
+    end
+  end
 
-  # def test_dice_values_do_not_change_unless_explicitly_rolled
-  #   dice = DiceSet.new
-  #   dice.roll(5)
-  #   first_time = dice.values
-  #   second_time = dice.values
-  #   assert_equal first_time, second_time
-  # end
+  def test_dice_values_do_not_change_unless_explicitly_rolled
+    dice = DiceSet.new
+    dice.roll(5)
+    first_time = dice.values
+    second_time = dice.values
+    assert_equal first_time, second_time
+  end
 
   def test_dice_values_should_change_between_rolls
     dice = DiceSet.new
@@ -50,10 +50,6 @@ class AboutDiceProject < Neo::Koan
     dice.roll(5)
     second_time = dice.values
 
-    puts first_time
-    puts "\n"
-    puts second_time 
-    puts "\n\n"
     assert_not_equal first_time, second_time,
       "Two rolls should not be equal"
 
